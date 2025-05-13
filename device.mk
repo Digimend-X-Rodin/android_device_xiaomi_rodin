13700759996ab258f75211e0425ee2639df03110
Bruno Martins <bgcngm@gmail.com>
2025-05-13 16:28:08 +0530

    duchamp: Migrate Lineage Health to soong_config_set
    Change-Id: I2eff39e83b9673aa2989d05de65c646241ba61da
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- device.mk
+++ device.mk
@@ -341,8 +341,14 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
 
 # Lineage Health
+$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
+$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/input_suspend)
+$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
+
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
+
 
 # PowerOffAlarm
 PRODUCT_PACKAGES += \
