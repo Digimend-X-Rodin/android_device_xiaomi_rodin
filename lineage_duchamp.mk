21ba02348751300df8bb219116586d4e38cfd9c0
Saikrishna1504 <saikrishna26918@gmail.com>
2025-07-21 18:54:22 +0530

    duchamp: Bringup @AxionAOSP
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- lineage_duchamp.mk
+++ lineage_duchamp.mk
@@ -11,19 +11,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/duchamp/device.mk)
 
-# Inherit some common VoltageOS stuff.
-$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Official-ify
-VOLTAGE_BUILD_TYPE := UNOFFICIAL
+# Axion Stuff
+AXION_CAMERA_REAR_INFO := 64,8,2
+AXION_CAMERA_FRONT_INFO := 16
+AXION_PROCESSOR := MTK_Dimensity_8300U
+AXION_MAINTAINER := Sai_Krishna
+TARGET_INCLUDES_LOS_PREBUILTS := true
+TARGET_INCLUDE_VIPERFX := true
+TARGET_ENABLE_BLUR := true
 
-# Boot animation
-TARGET_BOOT_ANIMATION_RES := 1920
+# Axion CPU Flags
+AXION_CPU_SMALL_CORES := 0,1,2,3
+AXION_CPU_BIG_CORES := 4,5,6,7
 
-# UDFPS animations
-EXTRA_UDFPS_ANIMATIONS := true
-
-PRODUCT_NAME := voltage_duchamp
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := lineage_duchamp
 PRODUCT_DEVICE := duchamp
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
@@ -36,4 +41,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="duchamp_global-user 15 AP3A.240905.015.A2 OS2.0.1.0.VNLMIXM release-keys" \
     BuildFingerprint=POCO/duchamp_global/duchamp:14/UP1A.230905.011/OS2.0.1.0.VNLMIXM:user/release-keys \
     DeviceProduct=$(PRODUCT_SYSTEM_NAME)
-
