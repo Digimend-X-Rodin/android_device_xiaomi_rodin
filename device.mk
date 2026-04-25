b03f72fe8692f5862aef692ee8ed7635f5800818
Omkar Parte <rakmoparte@gmail.com>
2026-04-25 14:36:07 +0530

    rodin: overlay: Import miui carrier configs from stock
    Reference: rodin_global-ota_full-OS3.0.10.0.WOJMIXM-user-16.0-7ea48324a1
    
    Signed-off-by: Omkar Parte <88646966+RAM-UNLOK@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -299,6 +299,7 @@ PRODUCT_COPY_FILES += \
 $(call inherit-product, hardware/mediatek/overlay/mssi.mk)
 
 PRODUCT_PACKAGES += \
+    CarrierConfigOverlayRodin \
     FrameworksResOverlayRodin \
     NfcOverlayRodin \
     SettingsResOverlayRodin \
